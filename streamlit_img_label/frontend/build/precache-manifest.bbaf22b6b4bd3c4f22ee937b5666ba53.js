085b6e375e4fd2d84257e4d8a218e886d64b5b21
yeemh <lkjjr0424@hanmail.net>
2022-04-21 17:44:01 +0900

    add build

---
+++ streamlit_img_label/frontend/build/precache-manifest.bbaf22b6b4bd3c4f22ee937b5666ba53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f24ba942e623cc308539e3a71189f25",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5c2a2c56d4f042da11dd",
+    "url": "./static/css/main.bfe20cfe.chunk.css"
+  },
+  {
+    "revision": "391f67c56ac7c23e705b",
+    "url": "./static/js/2.fe8319e6.chunk.js"
+  },
+  {
+    "revision": "4d420f862b878094dd984432a26bf30f",
+    "url": "./static/js/2.fe8319e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c2a2c56d4f042da11dd",
+    "url": "./static/js/main.d447f930.chunk.js"
+  },
+  {
+    "revision": "acc1e1b1767fa196d6fe",
+    "url": "./static/js/runtime-main.4a6ba311.js"
+  }
+]);
